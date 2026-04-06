b1d0fafc1dc4ebbd1f683315ad810e8ed0a95df0
S M Mahidur Rahman <166533655+MahidurSakib@users.noreply.github.com>
2026-04-06 15:26:41 +0600

    edited some parts

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -12,8 +12,9 @@ class AppServiceProvider extends ServiceProvider
     }
 
     public function boot(): void
-{
-    if (config('app.env') === 'production') {
-        \URL::forceScheme('https');
+    {
+        if (config('app.env') === 'production') {
+            \URL::forceScheme('https');
+        }
     }
 }
